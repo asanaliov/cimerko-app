6e328990113b833eddf3c6c3d352d76b6597b362
Asan <asanaliov01@gmail.com>
2026-06-29 22:14:56 +0200

    initial commit

---
+++ cimerko-app/Data/ApplicationDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace cimerko_app.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options) {
+}
